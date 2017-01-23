756a36c9879f2a76a7e960d0fa0ae4e8f5917d20
zveronline <zveronline@zveronline.ru>
2017-01-24 01:09:35 +0300

    .

--- lab1/script.py
+++ lab1/script.py
@@ -1,2 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.io
+
+data = scipy.io.loadmat('./data/1D/var3.mat')
+data = data['n']
+mean = np.mean(data)*np.ones(len(data))
+var = np.var(data)
+plt.plot(data)
+plt.show()
