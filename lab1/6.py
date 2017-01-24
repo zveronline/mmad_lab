59e764272ee3d25ccffb8ad6a0864c00d56dbabe
zveronline <zveronline@zveronline.ru>
2017-01-24 11:17:09 +0300

    .

---
+++ lab1/6.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+
+data = scipy.io.loadmat('./data/ND/var3.mat')
+data = data['mn']
--- lab1/script.py
+++ lab1/script.py
@@ -2,15 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io
 
-def autocorrelate(a):
-	n = len(a)
-	cor = []
-	for i in range(n//2, n//2+n):
-		a1 = a[:i+1] if i < n else a[i-n+1:]
-		a2 = a[n-i-1:] if i < n else a[:2*n-i-1]
-		cor.append(np.corrcoef(a1, a2)[0, 1])
-	return np.array(cor)
-
 data = scipy.io.loadmat('./data/1D/var3.mat')
 data = data['n']
 mean = np.mean(data)*np.ones(len(data))
