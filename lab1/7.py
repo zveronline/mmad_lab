bdc46a447cbf9644baa7497c8ad002e56145dbbe
zveronline <zveronline@zveronline.ru>
2017-01-24 17:10:22 +0300

    .

--- lab1/7.py
+++ lab1/7.py
@@ -9,3 +9,8 @@ corr_matrix = np.zeros([n, n])
 for i in range(0, n):
 	for j in range(0, n):
 		col = data[:, 3]
+		row = data[5, :]
+		range = data[2:5, 0:3]
+		data[:, -1]
+np.set_printoptions(precision=2)
+print(corr_matrix)
